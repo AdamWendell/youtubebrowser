3bbe8273f763d0980b8e86c31be629a303ebe6cf
Adam Wendell <adam@websmith.se>
2016-05-08 16:15:16 +0200

    Boilerplate for a youtubebrowser build with react

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const API_KEY = `AIzaSyAoP9P0lzVZQ4LErS27oR5EEqjbNmugjik`
+
+const App = () => {
+  return <div>Hi!</div>
+}
+
+ReactDOM.render( <App />, document.querySelector('.container'));
