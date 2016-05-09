f5fb089fe8d0f4f4494e4fb9a52b0b6a9a43e7b4
Adam Wendell <adam@websmith.se>
2016-05-09 23:40:00 +0100

    Completed application

--- src/index.js
+++ src/index.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import SearchBar from './components/SearchBar'
+import VideoList from './components/VideoList'
+import VideoDetail from './components/VideoDetail'
+import {debounce} from './helpers/helpers'
+import YTSearch from 'youtube-api-search'
 
 const API_KEY = `AIzaSyAoP9P0lzVZQ4LErS27oR5EEqjbNmugjik`
 
-const App = () => {
-  return <div>Hi!</div>
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {videos: [], selectedVideo: null}
+
+    this.videoSearch('surfboards')
+  }
+
+  videoSearch (term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({videos: videos, selectedVideo: videos[0]})
+    })
+  }
+  render () {
+    const videoSearch = debounce(term => this.videoSearch(term), 300)
+    return (
+      <div>
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo}/>
+      <VideoList onVideoSelect={selectedVideo => this.setState({selectedVideo})} videos={this.state.videos} />
+      </div>
+    )
+  }
 }
+
 
 ReactDOM.render( <App />, document.querySelector('.container'));
